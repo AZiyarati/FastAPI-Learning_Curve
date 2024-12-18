874fc9085ba7ca3a399ac7910af7be960fd3ac79
Pakrohk <rhpcir@gmail.com>
2024-12-18 08:03:40 +0330

    just beginning

--- getStart: PythonTypeIntro/motivation.py
+++ getStart: PythonTypeIntro/motivation.py
@@ -3,3 +3,10 @@ def get_full_name(first_name: str, last_name: str):
     return full_name
 
 print(get_full_name("john", "doe"))
+
+# More Motivation
+def get_name_with_age(name: str, age: int):
+    name_with_age = name + "is old" + str(age)
+    return name_with_age
+
+print(get_name_with_age("ali", 10))
