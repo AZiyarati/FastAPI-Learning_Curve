b7a2d45215cbfc7447ecae87b29ded522b378957
AZiyarati <ali.ziyarati25@gmail.com>
2024-12-19 04:05:08 +0330

    generic Types, list-tuple-set-dict-union

---
+++ getStart: PythonTypeIntro/03 - genericTypes.py
@@ -0,0 +1,15 @@
+# You can use the same builtin types as generics (with square brackets and types inside):
+
+# * list
+# * tuple
+# * set
+# * dict
+
+# And the same as with Python 3.8, from the typing module:
+
+# Union
+# Optional (the same as with Python 3.8)
+# ...and others.
+
+# In Python 3.10, as an alternative to using the generics "Union" and "Optional",
+#  you can use the vertical bar (|) to declare unions of types, that's a lot better and simpler.
