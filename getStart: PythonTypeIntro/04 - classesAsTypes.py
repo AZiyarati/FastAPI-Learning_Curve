e2c7c665c88a6cf539e20e6da9506a7248eac3bb
AZiyarati <ali.ziyarati25@gmail.com>
2024-12-19 04:20:31 +0330

    Use class as Type

--- getStart: PythonTypeIntro/04 - classesAsTypes.py
+++ getStart: PythonTypeIntro/04 - classesAsTypes.py
@@ -1,5 +1,6 @@
 # From Python 3.8-
 # In this example below, you can use class as type for function or variable.
+# example:
 class Person:
     def __init__(self, name: str):
         self.name = name
