b9026c36c63dfe4223b3aa5eba5aee2854f99d36
AZiyarati <ali.ziyarati25@gmail.com>
2024-12-25 11:52:18 +0330

    FastAPI beginning updated (added info)

--- userGuide/main.py
+++ userGuide/main.py
@@ -1,3 +1,11 @@
+'''
+for installing FastAPI:
+    1 - first we should make virtual envrionemnt (using uv or pip)
+    2 - pip install "fastapi[standard]"
+for run project for dev or standard :
+    dev : fastapi dev main.py
+    standard : fastapi run main.py
+'''
 from fastapi import FastAPI
 
 app = FastAPI()
